86deed4e8bcd3ae74b4df43f0e49136b1036bf9e
SOZIDANIE <vovan2izotow@gmail.com>
2022-06-23 22:19:44 +0500

    URAA_DODELAL

---
+++ app/src/main/java/com/bignerdranch/android/geomain/ekran5.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.geomain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ekran5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ekran5)
+    }
+}
